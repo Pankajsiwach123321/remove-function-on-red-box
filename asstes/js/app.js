2d1b7a6454b5f4237f870f0c7c18d75aba0239fb
Pankajsiwach123321 <pankusiwach000@gmai.com>
2023-10-09 11:10:45 +0530

    deleted extra line from js

--- asstes/js/app.js
+++ asstes/js/app.js
@@ -2,12 +2,9 @@ function remove() {
     var mybox_1 = document.getElementById("i");
     alert("you deleted box red  by function remove")
     mybox_1.remove();
-    mybox_2.style.display = "block";
-    alert("function remove()")
 }
 function hello() {
     var mybox_2 = document.getElementById("z");
     alert("you deleted box green by display none")
     mybox_2.style.display = "none";
-    alert("style.display.none")
 }
